794aec8f6c19804c24cda92cd827daf30c82289b
Valeri <valeria.chyornaya@gmail.com>
2020-11-23 18:19:53 +0200

    add main content

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../routes';
 import './Layout.css';
 
 const Layout = ({ children }) => (
   <div className="Layout">
     <header className="App-header">
-      <p>HEADER</p>
+      <h1>
+        <Link to={routes.MAIN}>AppCo</Link>
+      </h1>
     </header>
-    <hr />
     {children}
     <footer className="App-footer">
-      <p>FOOTER</p>
+      <span>
+        <Link to={routes.MAIN}>AppCo</Link>
+      </span>
+      <span>All rights reserved by ThemeTags</span>
+      <span>Copyrights © 2019.</span>
     </footer>
   </div>
 );
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,9 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Main = () => (
-  <div>
-    <h1>Main</h1>
-    <Link to="/stats">Users Statistic</Link>
+  <div className="Main">
+    <div className="banner">
+      <h2>Brainstorming for desired perfect Usability</h2>
+      <p>
+        Our design projects are fresh and simple and will benefit your business
+        greatly. Learn more about our work!
+      </p>
+      <Link to="/stats">Views Stats</Link>
+    </div>
+    <div className="mainpart-wrap">
+      <h3>Why small business owners love AppCo?</h3>
+      <p>
+        Our design projects are fresh and simple and will benefit your business
+        greatly. Learn more about our work!
+      </p>
+      <ul className="service-list">
+        <li>
+          <h4>Clean Design</h4>
+          <p>Increase sales by showing true dynamics of your website.</p>
+        </li>
+        <li>
+          <h4>Secure Data</h4>
+          <p>Build your online store’s trust using Social Proof & Urgency.</p>
+        </li>
+        <li>
+          <h4>Retina Ready</h4>
+          <p>
+            Realize importance of social proof in customer’s purchase decision.
+          </p>
+        </li>
+      </ul>
+      <div className="subscribe-form">
+        <input></input>
+        <button>Subscribe</button>
+      </div>
+    </div>
   </div>
 );
 
